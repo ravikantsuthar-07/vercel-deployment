871834dcd58d6d7efe655cb9c587b0ea81781d52
Ravi <ravi@gmail.com>
2025-04-02 11:23:50 +0530

    Frist1

--- backend/Config/D.js
+++ backend/Config/D.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await mongoose.connect(process.env.MONGODB_URL, {});
+        console.log(`MongoDB Connected`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
