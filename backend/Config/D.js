710f5d38fe3e99e7bfe120a8c5bdd8cc11d39362
Ravi <ravi@gmail.com>
2025-04-02 09:51:50 +0530

    Frist

--- backend/Config/D.js
+++ backend/Config/D.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose'
 
 const connectDB = async () =>{
     try {
-        const conn = mongoose.connect(process.env.MONGODB_URL);
+        const conn = mongoose.connect(process.env.MONGODB_URL, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
     } catch (error) {
         console.log(`error in MongoDB ${error}`);
     }
