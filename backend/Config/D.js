92bbcadb94b1ca4a4c86fb35d60c26d5553f287c
Ravi <ravi@gmail.com>
2025-04-02 10:17:59 +0530

    Frist

--- backend/Config/D.js
+++ backend/Config/D.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
