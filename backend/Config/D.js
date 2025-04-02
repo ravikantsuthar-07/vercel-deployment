d131e22192ac172801053350d4af4e43f3308c04
Ravi <ravi@gmail.com>
2025-04-02 10:10:24 +0530

    Frist

--- backend/Config/D.js
+++ backend/Config/D.js
@@ -2,15 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () =>{
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {
-            useCreateIndex: true, 
-            useFindAndModify: false, 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true 
-         }, err => {
-            if(err) throw err;
-            console.log('Connected to MongoDB!!!')
-         });
+        const conn = mongoose.connect(process.env.MONGODB_URL);
     } catch (error) {
         console.log(`error in MongoDB ${error}`);
     }
